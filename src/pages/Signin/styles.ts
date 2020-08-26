b95ecdae234da4bf9e168115286c08c939d09bdc
Marcus Pereira <marcus.work@hotmail.com>
2020-08-26 17:00:21 -0300

    Adding title to test the font

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4edeB;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
