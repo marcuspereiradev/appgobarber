6b7d7c048346bd1e355c4b3bf686d7d684905c36
Marcus Pereira <marcus.work@hotmail.com>
2020-08-30 15:09:57 -0300

    Adding content within the return

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -7,6 +7,7 @@ import {
   ScrollView,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -15,38 +16,40 @@ import logoImg from '../../assets/logo.png';
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
-const SignUp: React.FC = () => (
-  <>
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      enabled
-    >
-      <ScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-        keyboardShouldPersistTaps="handled"
+const SignUp: React.FC = () => {
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        <Container>
-          <Image source={logoImg} />
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
 
-          <View>
-            <Title>Crie sua conta</Title>
-          </View>
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
 
-          <Input name="name" icon="user" placeholder="Nome" />
-          <Input name="email" icon="mail" placeholder="E-mail" />
-          <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-          <Button onPress={() => {}}>Cadastrar</Button>
-        </Container>
-      </ScrollView>
-    </KeyboardAvoidingView>
+            <Button onPress={() => {}}>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
-    <BackToSignIn onPress={() => {}}>
-      <Icon name="arrow-left" size={20} color="#fff" />
-      <BackToSignInText>Voltar para logon</BackToSignInText>
-    </BackToSignIn>
-  </>
-);
+      <BackToSignIn onPress={() => {}}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
+};
 
 export default SignUp;
