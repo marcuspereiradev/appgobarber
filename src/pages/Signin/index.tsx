ef498c8376363534b9041e9734210982d3745528
Marcus Pereira <marcus.work@hotmail.com>
2020-09-10 14:47:28 -0300

    Importing @unform Form in the styles to overrite it css

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -8,7 +8,6 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
-import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
@@ -19,6 +18,7 @@ import logoImg from '../../assets/logo.png';
 import {
   Container,
   Title,
+  FormContent,
   ForgotPassword,
   ForgotPasswordText,
   CreateAccountButton,
@@ -29,7 +29,9 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: Object) => {}, []);
+  const handleSignIn = useCallback((data: Object) => {
+    console.log(data);
+  }, []);
 
   return (
     <>
@@ -49,7 +51,7 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={handleSignIn}>
+            <FormContent ref={formRef} onSubmit={handleSignIn}>
               <Input name="email" icon="mail" placeholder="E-mail" />
               <Input name="password" icon="lock" placeholder="Senha" />
 
@@ -60,7 +62,7 @@ const SignIn: React.FC = () => {
               >
                 Entrar
               </Button>
-            </Form>
+            </FormContent>
 
             <ForgotPassword onPress={() => {}}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
