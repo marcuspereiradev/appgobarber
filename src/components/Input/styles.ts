50c54be03ca34d68c1fc14f5242311535f857224
Marcus Pereira <marcus.work@hotmail.com>
2020-09-11 16:09:33 -0300

    Changing the color of the focus when filled or focused

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,15 +1,26 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+}
+export const Container = styled.View<ContainerProps>`
   align-items: center;
   background: #232129;
+  border-width: 2px;
+  border-color: #232129;
   border-radius: 10px;
   flex-direction: row;
   height: 60px;
   margin-bottom: 8px;
   padding: 0 16px;
   width: 100%;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
 `;
 
 export const TextInput = styled.TextInput`
