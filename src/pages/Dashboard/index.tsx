e0f6c50e78cc9672727bee2e767e19141846cd5a
Marcus Pereira <marcus.work@hotmail.com>
2020-09-14 15:31:58 -0300

    Styling Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Dashboard: React.FC = () => <View />;
+import { useAuth } from '../../hooks/auth';
+
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Container>
+      <View>
+        <Title>Dashboard</Title>
+      </View>
+      <Button onPress={signOut}>Sair</Button>
+    </Container>
+  );
+};
 
 export default Dashboard;
